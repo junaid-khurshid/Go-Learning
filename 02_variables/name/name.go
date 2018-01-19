90865a75b6ab7639b421185e63ab577a4afafab1
DESKTOP-FQMDMVD\Junaid <junaid381.sj@gmail.com>
2018-01-19 12:10:46 +0500

    variables nd packages

---
+++ 02_variables/name/name.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//var name = "junaid"
+
+func main() {
+	var name string
+	name = "juni"
+
+	fmt.Println("Hello", name)
+
+}
---
+++ 02_variables/shorthand/short.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "Hello Man!"
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+
+}
